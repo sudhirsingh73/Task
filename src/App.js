e406cfec8e3e387db385e852936aba981bc5c08b
sudhirsingh73 <sudhirsingh7303@gmail.com>
2023-05-31 21:31:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ReceiptFrom from './ReceiptFrom.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ReceiptFrom />
+    </div>
+  );
+}
+
+export default App;
